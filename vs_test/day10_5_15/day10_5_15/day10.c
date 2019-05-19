e6332b3afdd8acbd1515b8940472c1fec122589a
Madrid-7 <783798088@qq.com>
2019-05-19 11:04:12 +0800

    5_19

--- vs_test/day10_5_15/day10_5_15/day10.c
+++ vs_test/day10_5_15/day10_5_15/day10.c
@@ -1,9 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-
+#include<stdlib.h>
 
 int main()
 {
-
+	int a = 10;
+	printf("%d\n", a^1);
+	printf("%d\n", a|1);
+	printf("%d\n", a&1);
+	system("pause");
 	return 0;
 }
