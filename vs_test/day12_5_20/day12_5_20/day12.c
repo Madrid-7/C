01213f6ad56be56e39aa38ebf443ffc1799bc44e
Madrid-7 <783798088@qq.com>
2019-05-20 23:45:40 +0800

    ......5_20......

---
+++ vs_test/day12_5_20/day12_5_20/day12.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	printf("5.20\n");
+	printf("QY,没打算大学谈个男朋友?!想对你说我有点喜欢你了...\n");
+	system("pause");
+	return 0;
+}
