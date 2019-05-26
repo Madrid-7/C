ce4fbde5f5f523aa2e66694bb5e385eb8463caf7
Madrid-7 <783798088@qq.com>
2019-05-26 15:02:45 +0800

    5_26

--- vs_test/day12_5_20/day12_5_20/day12.c
+++ vs_test/day12_5_20/day12_5_20/day12.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<assert.h>
 //1.
 //编写函数：
 //unsigned int reverse_bit(unsigned int value);
@@ -29,8 +29,96 @@
 
 //2.
 //不使用（a + b） / 2这种方式，求两个数的平均值。
+//int ave_1(int x, int y)
+//{
+//	return y + ((x - y) >> 1);
+//}
+//int ave_2(int x, int y)
+//{
+//	return x & y + ((x ^ y) >> 1);
+//}
+//int main()
+//{
+//	int x = 0;
+//	int y = 0;
+//	scanf("%d%d", &x, &y);
+//	printf("ave_1 = %d\n", ave_1(x, y));
+//	printf("ave_2 = %d\n", ave_2(x, y));
+//	system("pause");
+//	return 0;
+//}
+
+//3.
+//编程实现：
+//一组数据中只有一个数字出现了一次。其他所有数字都是成对出现的。
+//请找出这个数字。（使用位运算）
+//int main()
+//{
+//	int arr[] = { 1, 5, 5, 1, 7, 6, 7, 8, 8, 9, 9 };
+//	int i = 0;
+//	int len = sizeof(arr) / sizeof(arr[0]);
+//
+//	for (i = 1; i < len; i++)
+//	{
+//		arr[0] = arr[0] ^ arr[i];                //按位异或同一个数两次不变
+//	}
+//	printf("%d\n", arr[0]);
+//	system("pause");
+//	return 0;
+//}
+
+//4.
+//有一个字符数组的内容为:"student a am i",
+//请你将数组的内容改为"i am a student".
+//要求：
+//不能使用库函数。
+//只能开辟有限个空间（空间个数和字符串的长度无关）。
+int lenth(char* str)
+{
+	int count = 0;
+	char* p = str;
+	while (*p != '\0')
+	{
+		count++;
+		p++;
+	}
+	return count;
+}
+void reverve_swap(char* left, char* right)
+{
+	assert(left != NULL);       //断言不是空指针
+	assert(right != NULL);
+	while (left < right)
+	{
+		char ret = *left;
+		*left = *right;
+		*right = ret;
+		left++;
+		right--;
+	}
+}
+void reverve(char* str)
+{
+	int len = lenth(str);
+	char* start = str;
+	char* end = NULL;
+	int i = 0;
+	reverve_swap(str, str + len - 1);
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == '\0' || str[i] == ' ')
+		{
+			end = str + i - 1;
+			reverve_swap(start, end);
+			start = str + i + 1;
+		}
+	}
+}
 int main()
 {
+	char str[] = "student a am i";
+	reverve(str);
+	printf("%s\n", str);
 	system("pause");
 	return 0;
 }
---
+++ vs_test/day12_5_20/day12_5_20/test.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//while (*str)
+//{
+//	start = str;
+//	while ((*str != ' ') && (*str != '\0')) //找到一块单词
+//	{
+//		str++;
+//	}
+//	end = str - 1;
+//	reverve_swap(start, end);//逆序单个单词
+//	if (*str == ' ') //找到空格
+//		str++;
+//}
