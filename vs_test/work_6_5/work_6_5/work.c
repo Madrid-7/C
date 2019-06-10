40a155fe82ab99133d15d367d7eae8f453c52ecf
Madrid-7 <783798088@qq.com>
2019-06-10 20:05:17 +0800

    6_10

---
+++ vs_test/work_6_5/work_6_5/work.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Stu
+{
+	int ranking;
+	char name[20];
+};
+int main()
+{
+
+	system("pause");
+	return 0;
+}
