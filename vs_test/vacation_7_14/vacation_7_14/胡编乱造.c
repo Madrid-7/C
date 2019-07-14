11efbd9d32ea1d584d6e00918d32d6ea08268d14
Madrid-7 <783798088@qq.com>
2019-07-15 00:34:15 +0800

    7_14

---
+++ vs_test/vacation_7_14/vacation_7_14/胡编乱造.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+
+//int main()
+//{
+//	int a[] = { 2,4,6,8,10,12,14,16,18,20,22,24 };
+//	int* q[4];
+//	int k = 0;
+//
+//	for (k = 0; k < 4; k++)
+//	{
+//		q[k] = &a[k * 3];
+//	}
+//	printf("%d\n", q[3][1]);
+//	system("pause");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i, j, a = 0;
+//	for (i = 0; i < 2; i++)
+//	{
+//		for (j = 0; j < 4; j++)
+//		{
+//			if (j % 2)
+//				break;
+//			a++;
+//		}
+//		a++;
+//	}
+//	printf("%d\n", a);
+//
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[4][5] ;     //   a[1]+3 == a[1][3]
+//
+//	system("pause");
+//	return 0;
+//}
+
+int main()
+{
+
+	system("pause");
+	return 0;
+}
