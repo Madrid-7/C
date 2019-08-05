bce354ab8de5d944f0e699c1ba1a2ab925af99fa
Madrid-7 <783798088@qq.com>
2019-08-05 10:26:13 +0800

    8_2

---
+++ vs_test/vacation_8_2/vacation_8_2/test.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdlib.h>
+#include<stdio.h>
+
+//如未声明，函数返回类型为int
+//max_num()
+//{
+//	return 777;
+//}
+//
+//int main()
+//{
+//	int n = max_num();
+//	printf("%d", n);
+//	system("pause");
+//	return 0;
+//}
+
+//int a[][2] = { 1,2,3,4 };                     //可定义二维数组
+//
+////int b[2][] = { {1 ,2}, {3 ,4} };             不可定义二维数组
+
+////结构体对齐
+//typedef struct {
+//	int a;     //4
+//	char b;    //1+1
+//	short c;    //2
+//	short d;    //2+2
+//}AA_t;               //12
+//
+//int main()
+//{
+//	AA_t new = { 1, 1, 3, 5 };
+//	printf("%d ", sizeof(new));
+//	system("pause");
+//	return 0;
+//}
+
+//char str[6] = { '\0' };
+//char str1[10];
+//str1 = "string";
+
+int main()
+{
+	char q = 1;
+	int w = 2;
+	float e = 3;
+	double r = 4;
+	int a = 10;
+	const char* p = "Hello!";
+	printf("%c\n", *p);         //H
+
+	a += a *= a -= a / 3;
+	printf("%d\n", a);         //98
+
+	a = q * w + r - e;
+
+	system("pause");
+	return 0;
+}
