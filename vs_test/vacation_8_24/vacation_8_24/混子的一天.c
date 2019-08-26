7f940ecd2e499ce23db51c252755d8d8d9e4e679
Madrid-7 <783798088@qq.com>
2019-08-26 22:42:12 +0800

    8_24

---
+++ vs_test/vacation_8_24/vacation_8_24/混子的一天.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdlib.h>
+#include<stdio.h>
+
+//int main()
+//{
+//
+//	char a = 1;
+//	int b = 1;
+//	float c = 1;
+//	double d = 1;
+//
+//	int x;
+////	x = a + b + c + d;             ֵΪdouble
+//	system("pause");
+//	return 0;
+//}
+
+int main()
+{
+
+	system("pause");
+	return 0;
+}
