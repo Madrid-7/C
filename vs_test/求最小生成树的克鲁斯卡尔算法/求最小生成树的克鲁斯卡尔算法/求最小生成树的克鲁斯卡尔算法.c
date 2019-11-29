d44d25608ae3a6d20ed96cd24517d6bab96fe78b
Madrid-7 <783798088@qq.com>
2019-11-29 23:59:53 +0800

    111.29

--- vs_test/求最小生成树的克鲁斯卡尔算法/求最小生成树的克鲁斯卡尔算法/求最小生成树的克鲁斯卡尔算法.c
+++ vs_test/求最小生成树的克鲁斯卡尔算法/求最小生成树的克鲁斯卡尔算法/求最小生成树的克鲁斯卡尔算法.c
@@ -17,7 +17,8 @@ typedef struct {
 	int begin;
 	int end;
 	int weight;
-} Vex;
+	int isUsed;
+} Edge;
 
 int LocateVex(AdjMatrix* G, char v)
 {
@@ -47,9 +48,9 @@ void Create(AdjMatrix *G)
 	{
 		scanf("%c", &G->vex[i]);
 	}
+	getchar();
 	for (i = 0; i < G->arcnum; i++)
 	{
-		getchar();
 		scanf("(%c,%c,%d)", &vex1, &vex2, &weight);
 		j = LocateVex(G, vex1);
 		k = LocateVex(G, vex2);
@@ -58,27 +59,63 @@ void Create(AdjMatrix *G)
 	}
 }
 
-void MiniSpanTree_Kruskal(AdjMatrix *G)
+int Find(int *parent, int f)
 {
-	Vex edge[MAXVEX];
+	//从下标f开始寻找parent数组中元素为0的下标
+	while (parent[f] > 0)
+	{
+		f = parent[f];
+	}
+	return f;
+}
+void Kruskal(AdjMatrix *G)
+{
+	int parent[MAXVEX] = { 0 };
+	Edge edge[MAXVEX];
+	int i, j;
 	int k = 0;
 	int n, m;
-	for (int i = 0; i < G->arcnum - 1; i++) {
-		for (int j = i + 1; j < G->arcnum; j++) {
-			if (G->arcs[i][j]) {
+	for (i = 0; i < G->vexnum- 1; i++) {
+		for (j = i + 1; j < G->vexnum; j++) {
+			if (G->arcs[i][j] != INF) {
 				edge[k].begin = i;
 				edge[k].end = j;
 				edge[k].weight = G->arcs[i][j];
+				edge[k].isUsed = 0;
 				k++;
 			}
 		}
 	}
-	for(i = 0; i < G->vexnum)
-
+	for (i = 0; i < k - 1; i++)
+	{
+		for (j = i + 1; j < k; j++)
+		{
+			if (edge[i].weight > edge[j].weight)
+			{
+				Edge tmp = edge[i];
+				edge[i] = edge[j];
+				edge[j] = tmp;
+			}
+		}
+	}
+	for (int i = 0; i < G->arcnum; i++)
+	{
+		n = Find(parent, edge[i].begin);
+		m = Find(parent, edge[i].end);
+		if (n != m)//m=n说明有环
+		{
+			parent[n] = m;
+			edge[i].isUsed = 1;
+		}
+		printf("(%c,%c,%d,%d)", G->vex[edge[i].begin], G->vex[edge[i].end], edge[i].weight, edge[i].isUsed);
+	}
 }
 
 int main()
 {
+	AdjMatrix* G = (AdjMatrix*)malloc(sizeof(AdjMatrix));
+	Create(G);
+	Kruskal(G);
 	system("pause");
 	return 0;
 }
